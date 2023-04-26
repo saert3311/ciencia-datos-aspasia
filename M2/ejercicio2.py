9e890cb08e7d5e8484620d5ce1cd3b1ac555728f
Jose Duarte <saert@outlook.com>
2023-04-26 01:01:10 -0400

    Enunciado

---
+++ M2/ejercicio2.py
@@ -0,0 +1,11 @@
+"""
+Manipular datos utilizando estructuras de vectores y matrices
+acorde a la biblioteca Numpy para resolver problemas
+"""
+
+"""
+Escribir un programa que pregunte al usuario por las ventas de un rango
+de años y muestre por pantalla una serie con los datos de las ventas
+indexada por los años, antes y después de aplicarles un descuento del
+10%.
+"""
