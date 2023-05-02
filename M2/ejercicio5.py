8b77aa5470cadac3993ecc58f97380d20ada9954
Jose Duarte <saert@outlook.com>
2023-05-02 19:22:16 -0400

    Inicio ejercicio 5 y csv

---
+++ M2/ejercicio5.py
@@ -0,0 +1,17 @@
+"""
+1. Una Tabla de BINGO
+
+Para una tarde de bingo se usarán tablas generadas con las siguientes
+reglas:
+● Los 5 números en cada columna son aleatorios ordenados no
+repetidos de acuerdo al rango asignado por columna.
+● La casilla central de la tabla tiene valor marcado cero o ‘libre’.
+Realice un programa para generar una tabla de bingo.
+
+B = entre 1 - 15
+I = entre 16 - 30
+N = entre 31 - 45
+G = entre 46 - 50
+O = entre 61 - 75
+"""
+
